c52ab765a365df3f2ba8a3f5fe3daa3781d510c6
Noizersam-sheng <noizersam@163.com>
2022-08-23 17:05:55 +0800

    神经网络模型

--- FullConnNet.py
+++ FullConnNet.py
@@ -4,10 +4,10 @@ import torch.nn as nn
 class FullConnNet(nn.Module):
     def __init__(self, n_feature):
         super(FullConnNet, self).__init__()
-        self.h1 = nn.Sequential(nn.Linear(n_feature, 16), nn.BatchNorm1d(16), nn.ReLU(True)) # Sequential相当于将网络层顺序拼接
-        self.h2 = nn.Sequential(nn.Linear(16, 32), nn.BatchNorm1d(32), nn.ReLU(True))
-        self.h3 = nn.Sequential(nn.Linear(32, 64), nn.BatchNorm1d(64), nn.Sigmoid())
-        self.h4 = nn.Linear(64, n_feature)
+        self.h1 = nn.Sequential(nn.Linear(n_feature, 256), nn.BatchNorm1d(256), nn.ReLU(True)) # Sequential相当于将网络层顺序拼接
+        self.h2 = nn.Sequential(nn.Linear(256, 64), nn.BatchNorm1d(64), nn.ReLU(True))
+        self.h3 = nn.Sequential(nn.Linear(64, 16), nn.BatchNorm1d(16), nn.ReLU(True))
+        self.h4 = nn.Linear(16, n_feature)
 
     def forward(self, x):
         x = self.h1(x)
